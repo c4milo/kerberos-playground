03a3073458b18495d895d94457ac5f4d0742a1e8
Camilo Aguilar <camilo.aguilar@enigma.com>
2017-12-28 15:25:35 -0500

    Replaces kinit with krbcontext package

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import requests
-from requests_kerberos import HTTPKerberosAuth, REQUIRED
+from krbcontext import krbContext
 
-kerberos_auth = HTTPKerberosAuth(mutual_authentication=REQUIRED, sanitize_mutual_error_response=False)
-r = requests.get("http://files.enigma.com:8080/blah", auth=kerberos_auth)
+from requests_kerberos import HTTPKerberosAuth, OPTIONAL 
+
+with krbContext(using_keytab=True,
+           principal='INGESTION/asm.enigma.com',
+           keytab_file='ingestion.keytab'):
+  pass
+
+kerberos_auth = HTTPKerberosAuth(mutual_authentication=OPTIONAL, sanitize_mutual_error_response=False)
+r = requests.get("http://files.enigma.com/mydata.csv", auth=kerberos_auth)
+#r = requests.get("https://www.petmd.com/sites/default/files/petmd-cat-happy-10.jpg", auth=kerberos_auth)
 
 print(r.status_code)
 print(r.content)
